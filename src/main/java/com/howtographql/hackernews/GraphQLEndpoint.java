61b42240bcf338b113ff517a0e8741eb59e65f92
Emperor <daniel.cs20@gmail.com>
2018-06-17 13:53:56 +0200

    3rd part (DB connection)

--- src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -1,6 +1,8 @@
 package com.howtographql.hackernews;
 
 import com.coxautodev.graphql.tools.SchemaParser;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import graphql.schema.GraphQLSchema;
 import graphql.servlet.SimpleGraphQLServlet;
 
@@ -9,15 +11,21 @@ import javax.servlet.annotation.WebServlet;
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
 
+    private static final LinkRepository linkRepository;
+
+    static {
+        MongoDatabase mongo = new MongoClient().getDatabase("hackernews");
+        linkRepository = new LinkRepository(mongo.getCollection("links"));
+    }
+
     public GraphQLEndpoint() {
         super(buildSchema());
     }
 
     private static GraphQLSchema buildSchema() {
-        LinkRepository linkRepository = new LinkRepository();
         return SchemaParser.newParser()
                 .file("schema.graphqls")
-                .resolvers(new Query(linkRepository))
+                .resolvers(new Query(linkRepository), new Mutation(linkRepository))
                 .build()
                 .makeExecutableSchema();
     }
